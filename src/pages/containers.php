fd6be18e8c279673ec0fdbc4e9582f6740a28cd0
Evanlab02 <evanlabuschagne70@gmail.com>
2024-11-03 12:14:29 +0200

    chore: Initial project

--- src/pages/containers.php
+++ src/pages/containers.php
@@ -15,32 +15,7 @@
     <?php include("../components/NavMenu.php"); ?>
     <div class="dk-content">
         <h1>Containers</h1>
-        <?php
-        require '../vendor/autoload.php';
-
-        use GuzzleHttp\Client;
-        use GuzzleHttp\Handler\CurlHandler;
-        use GuzzleHttp\HandlerStack;
-
-        $handler = new CurlHandler();
-        $stack = HandlerStack::create($handler);
-
-        // Set up Guzzle to use the Unix socket
-        $client = new Client([
-            'base_uri' => 'http://host.docker.internal:2375/v1.47/', // Base URI for Docker API
-            'handler' => $stack,
-        ]);
-
-        try {
-            // Make a GET request to the Docker API to list images
-            $response = $client->request('GET', 'containers/json');
-
-            // Print response
-            echo $response->getBody();
-        } catch (Exception $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-        ?>
+        <?php include("../components/Containers.php"); ?>
     </div>
     <script src="/js/base.js"></script>
     <script src="/js/components/Menus.js"></script>
